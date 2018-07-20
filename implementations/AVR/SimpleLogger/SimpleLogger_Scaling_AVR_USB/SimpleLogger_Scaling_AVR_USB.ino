a08ca32c66d704a0296f640ed1c9883dfadaaa47
paulvangentcom <palkab29@gmail.com>
2018-07-20 12:26:36 +0200

    V0.6.1

--- implementations/AVR/SimpleLogger/SimpleLogger_Scaling_AVR_USB/SimpleLogger_Scaling_AVR_USB.ino
+++ implementations/AVR/SimpleLogger/SimpleLogger_Scaling_AVR_USB/SimpleLogger_Scaling_AVR_USB.ino
@@ -21,15 +21,24 @@
 
 // -------------------- User Settable Variables --------------------
 int8_t hrpin = 0; //Whatever analog pin the sensor is hooked up to
-int8_t mode = 6; /*Speed mode. \
-              0 for 100Hz, 1 for 200Hz, 2 for 250Hz, \
-              3 for 500Hz, 4 for 1000Hz 5 for 2000Hz, \
-              and 6 for custom. Custom mode is set through Serial.\
-              See documentation for details.*/              
+int8_t scale_data = 1; /*Uses dynamic scaling of data when set to 1, not if set to 0 \
+                       sampling speed over 1500Hz not recommended when scaling data 
+                       on 8-bit AVR (e.g. Arduino)*/
+int8_t mode = 0; /*Speed mode. \
+              0 means the "sample_rate" speed will be used \
+              1 means custom. Custom sampling rate is set through Serial after connect.\
+              See documentation for details.  */
+int16_t sample_rate = 100; /*should be 4Hz or more, over 2KHz on AVR not recommended.
+                           When using adaptive scaling: over 1.5KHz not recommended on AVR.
+                           Higher speeds attainable on 32-bit chipsets.
+                           Please see documentation for suggested limits and theoretical limits*/
+                          
 // -------------------- End User Settable Variables --------------------
 //Don't change values from here on unless you know what you're doing
-long fs;
+long scalingFactor;
 long timerValue;
+long t1;
+long t_end;
 
 // -------------------- Data Struct Definition--------------------
 struct dataBuffers 
@@ -43,25 +52,79 @@ struct dataBuffers
   int16_t buffer1State = 0;
 };
 
+struct workingDataContainer
+{
+  long curVal = 0;
+  int16_t rangeLow = 0;
+  int16_t rangeLowNext = 1024;
+  int16_t rangeHigh = 1023;
+  int16_t rangeHighNext = 1;
+  int16_t rangeCounter = 0;
+};
+
 // -------------------- Data Struct Init --------------------
 struct dataBuffers dataBuf;
+struct workingDataContainer workingData;
 
 // -------------------- Functions --------------------
 void readSensors(struct dataBuffers &dataBuf)
 { //read the sensors, put in correct buffer
+  workingData.curVal = analogRead(hrpin);
+  
+  if(scale_data)
+  { //scale the data if requested
+    establish_range(workingData);
+    workingData.curVal = mapl(workingData.curVal, workingData.rangeLow, workingData.rangeHigh, 1, 1024);  
+    if(workingData.curVal < 0) workingData.curVal = 0;
+  }
+  
+  //put sensor value in correct buffer
   if (dataBuf.bufferMarker == 0) 
   {
-    dataBuf.hrdata0[dataBuf.bufferPointer] = analogRead(hrpin);
+    dataBuf.hrdata0[dataBuf.bufferPointer] = workingData.curVal;
   } else 
   {
-    dataBuf.hrdata1[dataBuf.bufferPointer] = analogRead(hrpin);
+    dataBuf.hrdata1[dataBuf.bufferPointer] = workingData.curVal;
   }
   dataBuf.bufferPointer++;
+}
+
+void establish_range(struct workingDataContainer &workingData)
+{
+  if(workingData.rangeCounter <= scalingFactor)
+  {
+    //update upcoming ranges
+    if(workingData.rangeLowNext > workingData.curVal) workingData.rangeLowNext = workingData.curVal;
+    if(workingData.rangeHighNext < workingData.curVal) workingData.rangeHighNext = workingData.curVal;
+    workingData.rangeCounter++;
+  } else {
+    //set range, minimum range should be bigger than 50
+    //otherwise set to default of (0, 1024)
+    if((workingData.rangeHighNext - workingData.rangeLowNext) > 50)
+    {
+      //update range
+      workingData.rangeLow = workingData.rangeLowNext;
+      workingData.rangeHigh = workingData.rangeHighNext;
+      workingData.rangeLowNext = 1024;
+      workingData.rangeHighNext = 1;      
+    } else {
+      //reset range to default
+      workingData.rangeLow = 0;
+      workingData.rangeHigh = 1024;
+    }
+    workingData.rangeCounter = 0;
+  }
+}
+
+long mapl(long x, long in_min, long in_max, long out_min, long out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 void getOCR(long fs)
 { // Calculate timer compare flag value
   timerValue = ((F_CPU / (64 * fs)) - 1);
+  scalingFactor = 2 * fs;
   if(timerValue >= 65535 || timerValue < 1)
   {
     Serial.println("Error, timer value incorrect.");
@@ -80,29 +143,14 @@ void setTimerInterrupts(int8_t mode)
   switch(mode)
   {
     case 0:
-      timerValue = 2499;
+      getOCR(sample_rate);
       break;
-    case 1:
-      timerValue = 1249;
-      break;
-    case 2:
-      timerValue = 999;
-      break;
-    case 3:
-      timerValue = 499;
-      break;
-    case 4:
-      timerValue = 249;
-      break;
-    case 5:
-      timerValue = 124;
-      break;
-    case 6:
+    case 1:      
       Serial.println("Logger ready!");
       while(Serial.available() == 0){}
-      fs = Serial.parseInt();
+      sample_rate = Serial.parseInt();
       Serial.flush();
-      getOCR(fs);
+      getOCR(sample_rate);
       break;
   };
   //set-up the interrupts on the 328p
@@ -120,7 +168,10 @@ void setTimerInterrupts(int8_t mode)
 
 ISR(TIMER1_COMPA_vect)
 { //define interrupt service routine
+  //t1 = micros();
   readSensors(dataBuf);
+  //t_end = micros() - t1;
+  //Serial.println(t_end);
 }
 
 // -------------------- Setup --------------------
